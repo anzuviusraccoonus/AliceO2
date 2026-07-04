c2c0bae1b233f7a8b5ab323280d86194f443b234
NNathanson <63293077+NNathanson@users.noreply.github.com>
2026-07-04 11:43:32 +0200

    changed to care about first l1 line only

--- Detectors/FOCAL/reconstruction/include/FOCALReconstruction/HCalDecoder.h
+++ Detectors/FOCAL/reconstruction/include/FOCALReconstruction/HCalDecoder.h
@@ -51,8 +51,10 @@ class HCalDecoder {
   bool mL1FilterMode = false;
   int mIdleBeforeL1Count_Link0;
   int mIdleBeforeL1_Link0;
+  bool mFoundL1_Link0;
   int mIdleBeforeL1Count_Link1;
   int mIdleBeforeL1_Link1;
+  bool mFoundL1_Link1;
   //hardcode the changes here once the distances have been determined and change the filter mode to true
   int mMaxL1Dist_Link0 = 0;
   int mMaxL1Dist_Link1 = 0;
